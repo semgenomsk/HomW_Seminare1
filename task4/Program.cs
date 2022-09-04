fdc2f0fe43108185c5d126001665515ced6f9ede
Semen <semgenomsk@yandex.ru>
2022-09-04 22:42:47 +0600

    Done task4

---
+++ task4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b>max)
+{
+    max=b;
+}
+if (c>max)
+{
+    max=c;
+}
+Console.WriteLine("max = " + max);      
